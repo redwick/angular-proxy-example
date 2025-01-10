7721f549e25551ad51f2978de3bd940948a7daf7
Bogdan Isaev <redzor@live.com>
2025-01-10 11:36:37 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {environment as env} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ng19';
+  title = 'ng-proxy';
+  constructor(private http: HttpClient) {
+    this.http.get(env.apiUrl + 'time', {responseType: 'text'}).subscribe(data => {
+      alert(data);
+    });
+  }
 }
