e70012112ffc52ca05476b8476bf2d57ad76fedf
Bogdan Isaev <redzor@live.com>
2025-01-13 15:32:23 +0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,20 @@ import {environment as env} from '../environments/environment';
 })
 export class AppComponent {
   title = 'ng-proxy';
+  proxy_data = '';
+  direct_data = '';
   constructor(private http: HttpClient) {
+    this.getWithProxy();
+    this.getWithDirect();
+  }
+  getWithProxy(){
     this.http.get(env.apiUrl + 'time', {responseType: 'text'}).subscribe(data => {
-      alert(data);
+      this.proxy_data = data;
+    });
+  }
+  getWithDirect(){
+    this.http.get('https://djarviss.ru/rest/' + 'time', {responseType: 'text'}).subscribe(data => {
+      this.direct_data = data;
     });
   }
 }
